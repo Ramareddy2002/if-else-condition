7bfc9d37924ae7db13e487a2b9fe66fa01bc61f6
Ramareddy2002 <84448206+Ramareddy2002@users.noreply.github.com>
2021-05-22 12:31:24 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,24 @@
+''' python program to print a person is eligible for vote or not'''
+
+
+
+name=raw_input("enter input ") 
+age=int(raw_iput(" enter age")) 
+if age>=18:
+          print (" Hi", name) 
+          print("eligible for vote") 
+else:
+    print("sorry", name) 
+    print ("not eligible for vote") 
+    
+    
+    
+    '''
+    
+ expected out put
+ enter input rama reddy
+ enter age 20
+ Hi rama reddy
+ eligible for vote
+  '''
+          
